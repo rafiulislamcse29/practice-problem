e5c6c907b158e3402054a34201da91e7cfb9154c
Md Rafiul Islam (rafiulislamcse29@gmail.com) <167911988+rafiulislamcse29@users.noreply.github.com>
2024-05-23 16:56:55 +0600

    routing

---
+++ project5_Page_Navigation/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def about(request):
+  return render(request, 'first_app/about.html')
+
+def contact(request):
+  return render(request, 'first_app/contact.html')
